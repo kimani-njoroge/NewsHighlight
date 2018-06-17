3a7f2f02bb60711f7fb3e12dadcdcb7d6a3455c2
kimani-njoroge <kimani064@gmail.com>
2018-06-17 10:02:41 +0300

    add new configurations and connect it to the app

--- app/__init__.py
+++ app/__init__.py
@@ -5,6 +5,6 @@ from .config import DevConfig
 app = Flask(__name__, instance_relative_config=True)
 
 app.config.from_object(DevConfig)
-# app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py')
 
 from app import views
