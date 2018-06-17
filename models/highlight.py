991dad3d2e6be98383a3940c61cf1b009cf92d26
kimani-njoroge <kimani064@gmail.com>
2018-06-17 10:07:54 +0300

    create a highlight class

---
+++ models/highlight.py
@@ -0,0 +1,9 @@
+class Highlight:
+    '''
+    highlight class to define objects
+    '''
+
+    def __init__(self, id, name, category):
+        self.id = id
+        self.name = name
+        self.category = category
