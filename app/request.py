deb9677f3f41f63d8f6714dd01b3d99e02a93620
kimani-njoroge <kimani064@gmail.com>
2018-06-17 10:15:13 +0300

    access configuration to get base url

--- app/request.py
+++ app/request.py
@@ -1,3 +1,8 @@
 from app import app
+import urllib.request, json
+from models import highlight
+
+Highlight = highlight.Highlight
 
 api_key = app.config['NEWS_API_KEY']
+base_url = app.config["NEWS_API_BASE_URL"]
