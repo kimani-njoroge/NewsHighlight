77b8c18d5f6cbe94613ebf88bc0260de9032c10b
kimani-njoroge <kimani064@gmail.com>
2018-06-17 10:22:05 +0300

    create variable business articles and business highlights

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from flask import render_template
 from app import app
-# from .request import get_highlights
+from .request import get_highlights
 
 # Views
 @app.route('/')
@@ -9,13 +9,13 @@ def index():
     '''
     View root page function that returns the index page and its data
     '''
-    # business_articles = get_highlights('business')
+    business_articles = get_highlights('business')
     # print(business_articles)
     # technology = get_highlights('technology')
     # entertainment = get_highlights('entertainment')
     title = 'NewsHighlights'
     # message = 'Welcome to news highlight'
-    return render_template('index.html', title=title,) # business=business_articles)
+    return render_template('index.html', title=title, business=business_articles)
 
 app.route('/sources/<int:sources_id>')
 def source(sources_id):
